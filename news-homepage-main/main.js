082ba0dc6e8f723f25b4bf07c5ff623561b76b66
Virgile <100997601+virgile-lab@users.noreply.github.com>
2022-11-22 13:24:43 +0100

    test

---
+++ news-homepage-main/main.js
@@ -0,0 +1,6 @@
+var menuIcon = document.querySelector(".menu-icon");
+var screenFilter = document.querySelector(".screen-filter");
+var bodyDocument = document.querySelector("body");
+
+displayMenu =
+    console.log(menuIcon, screenFilter, bodyDocument);
