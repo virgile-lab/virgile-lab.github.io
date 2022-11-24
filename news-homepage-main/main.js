b514ea5900b3aa44ab94812cc121fa7bae9a93a3
Virgile <mail.liverig@gmail.com>
2022-11-24 19:26:07 +0100

    commit

--- news-homepage-main/main.js
+++ news-homepage-main/main.js
@@ -2,7 +2,22 @@ const toggleMenu = document.querySelector(".toggle-menu");
 const primaryNav = document.querySelector(".primary-nav");
 const bodyDocument = document.querySelector("body");
 const screenOverlay = document.querySelector(".screen-overlay");
+var viewportWidth = window.innerWidth;
+var a = false;
 
+function vSize() {
+    window.onresize = function() {
+        viewportWidth = window.innerWidth;
+        if (viewportWidth >= 768) {
+            console.log("sup");
+            bodyDocument.style.overflowY = "scroll";
+            primaryNav.setAttribute('data-visible', 'false');
+
+        }
+    }
+}
+
+vSize();
 
 toggleMenu.addEventListener('click', () => {
 
