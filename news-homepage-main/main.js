318a9a7e6af6fa38c8cfb21c828da2a618e2c110
Virgile <mail.liverig@gmail.com>
2022-11-24 15:58:10 +0100

    commit

--- news-homepage-main/main.js
+++ news-homepage-main/main.js
@@ -1,33 +1,26 @@
 const toggleMenu = document.querySelector(".toggle-menu");
 const primaryNav = document.querySelector(".primary-nav");
-const menu = primaryNav.querySelector("ul");
 const bodyDocument = document.querySelector("body");
 const screenOverlay = document.querySelector(".screen-overlay");
 
 
 toggleMenu.addEventListener('click', () => {
 
-        var visibility = primaryNav.getAttribute('data-visible');
+    var visibility = primaryNav.getAttribute('data-visible');
 
-        if (visibility === 'false') {
-            bodyDocument.style.overflowY = "hidden";
-            menu.style.display = "flex";
-            setTimeout(() => {
-                primaryNav.setAttribute('data-visible', 'true');
-            });
-
-        } else {
-            bodyDocument.style.overflowY = "scroll";
-            primaryNav.setAttribute('data-visible', 'false');
-            setTimeout(() => {
-                menu.style.display = "none";
-
-            }, 1001);
-        }
-
+    if (visibility === 'false') {
+        bodyDocument.style.overflowY = "hidden";
+        primaryNav.setAttribute('data-visible', 'true');;
+    } else {
+        bodyDocument.style.overflowY = "scroll";
+        primaryNav.setAttribute('data-visible', 'false');
     }
 
-)
+});
+
 screenOverlay.addEventListener('click', () => {
+
+    bodyDocument.style.overflowY = "scroll";
     primaryNav.setAttribute('data-visible', 'false');
-})
+
+});
